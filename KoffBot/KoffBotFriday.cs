046021748d0f1890ac5f33349c646174f4f33dab
Iiro Vanninen <iiro.vanninen@zure.com>
2020-10-21 15:26:01 +0300

    Moved Slack webhook to key vault

--- KoffBot/KoffBotFriday.cs
+++ KoffBot/KoffBotFriday.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
@@ -10,18 +9,16 @@ namespace KoffBot
     public static class KoffBotFriday
     {
         [FunctionName("KoffBotFriday")]
-        public static async Task<IActionResult> Run([TimerTrigger("0 1 0 * * 5")] TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 1 0 * * 5")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation("KoffBot activated. Ready to hail friday.");
 
             var client = new HttpClient();
-            var content = new HttpRequestMessage(HttpMethod.Post, "https://hooks.slack.com/services/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")
+            var content = new HttpRequestMessage(HttpMethod.Post, System.Environment.GetEnvironmentVariable("SlackWebHook"))
             {
                 Content = new StringContent("{\"text\": \"It's friday! :meow_party: You should celebrate with a Koff! :koff:\" }", Encoding.UTF8, "application/json")
             };
             await client.SendAsync(content);
-
-            return new OkResult();
         }
     }
 }
--- KoffBot/KoffBotToast.cs
+++ KoffBot/KoffBotToast.cs
@@ -1,12 +1,9 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System.Net.Http;
 using System.Text;
 
@@ -22,7 +19,7 @@ namespace KoffBot
             log.LogInformation("KoffBot activated. Ready for furious toasting.");
 
             var client = new HttpClient();
-            var content = new HttpRequestMessage(HttpMethod.Post, "https://hooks.slack.com/services/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")
+            var content = new HttpRequestMessage(HttpMethod.Post, System.Environment.GetEnvironmentVariable("SlackWebHook"))
             {
                 Content = new StringContent("{\"text\": \"Koff!\" }", Encoding.UTF8, "application/json")
             };
