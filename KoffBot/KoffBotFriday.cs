ada9c1676c4e8ffd4c0b67550f2aefedba1e92c7
Iiro Vanninen <iiro.vanninen@zure.com>
2020-10-22 15:01:28 +0300

    Added randomization of Friday messages

--- KoffBot/KoffBotFriday.cs
+++ KoffBot/KoffBotFriday.cs
@@ -3,20 +3,27 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using System.Text;
+using System;
 
 namespace KoffBot
 {
     public static class KoffBotFriday
     {
         [FunctionName("KoffBotFriday")]
+#if DEBUG
+        public static async Task Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+#else
         public static async Task Run([TimerTrigger("0 1 0 * * 5")] TimerInfo myTimer, ILogger log)
+#endif
         {
             log.LogInformation("KoffBot activated. Ready to hail friday.");
 
             var client = new HttpClient();
-            var content = new HttpRequestMessage(HttpMethod.Post, System.Environment.GetEnvironmentVariable("SlackWebHook"))
+            Random random = new Random();
+            int randomIndex = random.Next(0, Messages.FridayPossibilities.Length);
+            var content = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SlackWebHook"))
             {
-                Content = new StringContent("{\"text\": \"Se on perjantai! :meow_party: Mikset juhlisi ottamalla Koff! :koff:\" }", Encoding.UTF8, "application/json")
+                Content = new StringContent("{\"text\": \"" + Messages.FridayPossibilities[randomIndex] + "\" }", Encoding.UTF8, "application/json")
             };
             await client.SendAsync(content);
         }
---
+++ KoffBot/Messages.cs
@@ -0,0 +1,16 @@
+﻿namespace KoffBot
+{
+    public static class Messages
+    {
+        public static string[] FridayPossibilities = new string[]
+        {
+            "Se on perjantai! :meow_party: Mikset juhlisi ottamalla Koff! :koff:",
+            "Perjantai! :koff: :koff: :koff:",
+            "Jouni, tänään on perjantai! Jallua saa ottaa, mutta ota mieluummin Koff! :koff:",
+            "Onko se taas perjantai? :meow_party: Koff!",
+            "Miten niin liian aikaista oluelle? Nyt on perjantai! Koff!",
+            "Mitä olisi elämä ilman perjantaita? Pelkkää työtä! Joten ole tänään kiitollinen perjantaista ja ota yksi (tai useampi) Koff!"
+            "Perjantai, päivien aatelinen! Koff! Koff! :koff:"
+        };
+    }
+}
