1bfb7405f4120ac1cb7a4512712c1edf808f7538
Iiro Vanninen <iiro.vanninen@zure.com>
2023-01-24 11:52:14 +0200

    More friday messages

--- KoffBot/Messages.cs
+++ KoffBot/Messages.cs
@@ -15,6 +15,9 @@ public static class Messages
         "Perjantai, perjantai, perjantai. Perjantai! :koff:",
         "Muistatteko 90-luvun radiopätkän: 'Miksei aina voi olla perjantai?' Niinpä! :koff: :koff: :koff:",
         "Perjantai 13. päivä vuonna 1307 oli synkkä temppeliherrojen ritarikunnalle. Heidät vangittiin ja lähes kaikkia kidutettiin brutaalisti. Yksi yleisimmistä tämän ajan metodeista oli ns. 'Strappdo': Kidutettava nostettiin ilmaan pelkkien ranteidensa varassa kunnes heidän olkapäänsä popsahtivat irti luu-aukoistaan. Lopuksi temppeliritarit poltettiin roviolla. Liekeissä heille olisi varmasti maistunut yksi janon sammuttava Koff! :koff:",
-        "Perjantai, eli: :koff: K :koff: O :koff: F :koff: F :koff:"
+        "Perjantai, eli: :koff: K :koff: O :koff: F :koff: F :koff:",
+        "Perjantai! Onko se aftereille, keikalle vai yökerholle? Sen kaiken kertoo :koff:",
+        "Perjantai! :koff: Perjantai! :koff: Perjantai!!! :koff: :koff: :koff:",
+        "Onko taas mennyt liian vähän kaljaa? Älä sure, tänään on perjantai! Koff! :koff:"
     };
 }
