7abd04dfb51861f1a23d7648cc93ff5f5c9aba82
Iiro Vanninen <iiro.vanninen@zure.com>
2023-10-25 21:41:19 +0300

    Testing with different settings

--- KoffBot/Database/KoffBotContext.cs
+++ KoffBot/Database/KoffBotContext.cs
@@ -23,10 +23,6 @@ public partial class KoffBotContext : DbContext
 
     public virtual DbSet<LogToast> LogToasts { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress;Database=KoffBot;Trusted_Connection=True;TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<LogDrunk>(entity =>
