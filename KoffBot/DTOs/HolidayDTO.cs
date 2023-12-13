f567d46821c9c7d50d2b6e519482052fe3f8b99e
Iiro Vanninen <iiro.vanninen@zure.com>
2023-12-13 11:27:24 +0200

    Removed old holiday DTO

--- KoffBot/DTOs/HolidayDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KoffBot;
-
-public class HolidayDto
-{
-    public DateTime Date { get; set; }
-    public string Name { get; set; }
-    public string Url { get; set; }
-    public string Description { get; set; }
-}
