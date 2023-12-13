5c6360575d18209583424b69090798120652bbba
Iiro Vanninen <iiro.vanninen@zure.com>
2023-12-13 11:27:37 +0200

    Added new holiday function

---
+++ KoffBot/DTOs/HolidayInboundApiDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KoffBot;
+
+public class HolidayInboundApiDto
+{
+    [JsonPropertyName("specialDates")]
+    public List<HolidayInboundApiDetailsDto> SpecialDates { get; set; }
+}
+
+public class HolidayInboundApiDetailsDto
+{
+    [JsonPropertyName("date")]
+    public string Date { get; set; }
+    [JsonPropertyName("nameFI")]
+    public string Name { get; set; }
+}
---
+++ KoffBot/DTOs/HolidaySlackMessageDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace KoffBot;
+
+public class HolidaySlackMessageDto
+{
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+}
