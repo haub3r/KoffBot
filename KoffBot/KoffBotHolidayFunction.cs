4ecc59ac012800f7915b4d93c5f1ea40c5bde466
Iiro Vanninen <iiro.vanninen@zure.com>
2024-02-14 11:46:59 +0200

    Refactored holiday message style

--- KoffBot/KoffBotHolidayFunction.cs
+++ KoffBot/KoffBotHolidayFunction.cs
@@ -1,4 +1,5 @@
 ﻿using KoffBot.Dtos;
+using KoffBot.Messages;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using System.Globalization;
@@ -39,7 +40,8 @@ public class KoffBotHolidayFunction
                 continue;
             }
 
-            var slackMessage = ResolveHolidayMessage(holiday.Name);
+            var foundHoliday = HolidayMessages.HolidayPossibilities.Where(m => m.Key == holiday.Name).FirstOrDefault();
+            var slackMessage = foundHoliday.Value ?? "";
             var message = new HolidaySlackMessageDto
             {
                 Text = slackMessage
@@ -52,22 +54,5 @@ public class KoffBotHolidayFunction
             await httpClient.SendAsync(content);
             break;
         }
-    }
-
-    private static string ResolveHolidayMessage(string holidayName)
-    {
-        return holidayName switch
-        {
-            "Uudenvuodenpäivä" => "Hyvää uuttavuotta! Eilen taisi mennä muutama Koff! :koff: Uuteen nousuun? Koff!",
-            "Pitkäperjantai" => "Hyvää pitkäperjantaita! Kristus uhrautui jotta saisimme yhden vapaapäivän lisää! :buddy-jesus: Koff!",
-            "Toinen pääsiäispäivä" => "Pääsiäinen! Jeesus on ylösnoussut! Käy siis korkkaamassa Hänen kanssaan yksi Koff! :koff: Mutta älä kerro Hänen faijalleen :shushing_face: :koff:",
-            "Vappu" => "Työläisten päivä eli vappu! Ota siis sen kunniaksi ainakin yksi Koff! (Sinebrychoff ei tunnista vappua kansalliseksi juhlapäiväksi eikä päivän nimellinen merkitys oikeuta työntekijää pitämään vapaapäivää. Poissaolo päivän aikana lasketaan työsopimuksen vastaiseksi ja voi johtaa työntekijän välittömään irtisanomiseen työtehtävistä. Tämän viestin lukeminen ei oikeuta työntekijää pitämään taukoa.)",
-            "Helatorsta" => "Hyvää helatorstaita! Tällä päivällä ei ole mitään merkitystä, kuten ei sinullakaan! Aika avata yksi Koff? :koff:",
-            "Juhannusaatto" => "Hyvää juhannusta! :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff:",
-            "Itsenäisyyspäivä" => "Hyvää itsenäisyyspäivää! Perhe, isänmaa ja Koff! :flag-fi: :koff: :ak47:",
-            "Jouluaatto" => "Hyvää ja onnellista joulua toivottaa KoffBot! :santa: :koff:",
-            "Tapaninpäivä" => "Tapaninpäivä on ensimmäisen marttyyrin Stefanoksen ja myös kaikkien muiden marttyyrien muistopäivä. Suomessa tapaninpäivästä on vuosien saatossa tullut myös hevosten ja hevosmiesten juhlapäivä. Mikä siis parempi päivä laittaa joululahjarahat oikealle hevoselle! Mene osoitteeseen unibet.com ja syötä promokoodi 'KOFFBOTSPECIAL' tienaaksesi 5$ ilmaisina vedonlyöntikrediitteinä! Koff! :koff:",
-            _ => null
-        };
     }
 }
---
+++ KoffBot/Messages/HolidayMessages.cs
@@ -0,0 +1,17 @@
+﻿namespace KoffBot.Messages;
+
+public static class HolidayMessages
+{
+    public static Dictionary<string, string> HolidayPossibilities = new Dictionary<string, string>
+    {
+        {"Uudenvuodenpäivä", "Hyvää uuttavuotta! Eilen taisi mennä muutama Koff! :koff: Uuteen nousuun? Koff!"},
+        {"Pitkäperjantai", "Hyvää pitkäperjantaita! Kristus uhrautui jotta saisimme yhden vapaapäivän lisää! :buddy-jesus: Koff!"},
+        {"Toinen pääsiäispäivä", "Pääsiäinen! Jeesus on ylösnoussut! Käy siis korkkaamassa Hänen kanssaan yksi Koff! :koff: Mutta älä kerro Hänen faijalleen :shushing_face: :koff:"},
+        {"Vappu", "Työläisten päivä eli vappu! Ota siis sen kunniaksi ainakin yksi Koff! (Sinebrychoff ei tunnista vappua kansalliseksi juhlapäiväksi eikä päivän nimellinen merkitys oikeuta työntekijää pitämään vapaapäivää. Poissaolo päivän aikana lasketaan työsopimuksen vastaiseksi ja voi johtaa työntekijän välittömään irtisanomiseen työtehtävistä. Tämän viestin lukeminen ei oikeuta työntekijää pitämään taukoa.)"},
+        {"Helatorsta", "Hyvää helatorstaita! Tällä päivällä ei ole mitään merkitystä, kuten ei sinullakaan! Aika avata yksi Koff? :koff:"},
+        {"Juhannusaatto", "Hyvää juhannusta! :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff: :koff:"},
+        {"Itsenäisyyspäivä", "Hyvää itsenäisyyspäivää! Perhe, isänmaa ja Koff! :flag-fi: :koff: :ak47:"},
+        {"Jouluaatto", "Hyvää ja onnellista joulua toivottaa KoffBot! :santa: :koff:"},
+        {"Tapaninpäivä", "Tapaninpäivä on ensimmäisen marttyyrin Stefanoksen ja myös kaikkien muiden marttyyrien muistopäivä. Suomessa tapaninpäivästä on vuosien saatossa tullut myös hevosten ja hevosmiesten juhlapäivä. Mikä siis parempi päivä laittaa joululahjarahat oikealle hevoselle! Mene osoitteeseen unibet.com ja syötä promokoodi 'KOFFBOTSPECIAL' tienaaksesi 5$ ilmaisina vedonlyöntikrediitteinä! Koff! :koff:"},
+    };
+}
